f04b37a9b602ebf8f9ad9dc1cc51206c7bf29606
anil ekici <anilcanekici@gmail.com>
2020-11-05 15:09:42 +0000

    models completed

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  validates_uniqueness_of :cocktail_id, scope: [:ingredient_id]
+  # validates_uniqueness_of :ingredient_id, scope: [:cocktail_id]
 end
